291fd7b3a43206241f80449feb42f7ab4d6eff62
anilsjr <anil.vishwakarma.it@gmail.com>
2025-02-12 19:23:41 +0530

    comment added and enhance responsiveness

---
+++ README.md
@@ -0,0 +1,16 @@
+# live link
+[anilsjr.github.io/random-color](https://anilsjr.github.io/random-color/)
+# Random Color Generator
+
+This project is a web application that generates random colors and displays them in a grid of color boxes. Each color box shows the corresponding hexadecimal color code. The application is built using HTML, CSS, and JavaScript, and utilizes Bootstrap for styling. Users can see a variety of colors dynamically generated each time they load the page.
+
+## Features
+- Generates 30 random color boxes
+- Displays hexadecimal color codes
+- Responsive design with Bootstrap
+- Hover effects for color boxes
+
+## Technologies Used
+- HTML
+- CSS (with Bootstrap)
+- JavaScript
--- js/script.js
+++ js/script.js
@@ -1,28 +1,43 @@
+// Get the container element where color boxes will be added
 var container = document.getElementById("color-container");
 
-for (let index = 0; index < 30; index++) {
-    var colorBox = document.createElement("div");
-    var colorCode = document.createElement("span");
+// Create 30 color boxes and append them to the container
+function createColorBoxElement() {
+    for (let index = 0; index < 30; index++) {
+        var colorBox = document.createElement("div");
+        var colorCode = document.createElement("span");
 
-    colorBox.classList.add("col-5", "col-md-2", "p-3", "d-flex", "justify-content-center", "align-items-center", "color-box", "shadow");
-    colorCode.classList.add("color-code");
+        // Add necessary classes to the color box and color code elements
+        colorBox.classList.add("col-5", "col-md-2", "p-3", "d-flex", "justify-content-center", "align-items-center", "color-box", "shadow");
+        colorCode.classList.add("color-code");
 
-    colorBox.appendChild(colorCode);
-    container.appendChild(colorBox);
+        // Append the color code element to the color box
+        colorBox.appendChild(colorCode);
+        // Append the color box to the container
+        container.appendChild(colorBox);
+    }
+
+    // Select all color box elements
+    var colorContainerEls = document.querySelectorAll(".color-box");
+
+    // Function to generate random colors and apply them to the color boxes
+    function generateColor() {
+        colorContainerEls.forEach(colorBox => {
+            var newColorCode = randomColor();
+            colorBox.style.backgroundColor = "#" + newColorCode;
+            var colorCode = colorBox.querySelector(".color-code");
+            colorCode.textContent = "#" + newColorCode;
+        });
+    }
+
+    // Generate random colors for the color boxes
+    generateColor();
 }
 
-var colorContainerEls = document.querySelectorAll(".color-box");
-generateColor();
+// Call the function to create color boxes
+createColorBoxElement();
 
-function generateColor() {
-    colorContainerEls.forEach(colorBox => {
-        var newColorCode = randomColor();
-        colorBox.style.backgroundColor = "#" + newColorCode;
-        var colorCode = colorBox.querySelector(".color-code");
-        colorCode.textContent = "#" + newColorCode;
-    });
-}
-
+// Function to generate a random 6-character hexadecimal color code
 function randomColor() {
     const chars = "0123456789abcdef";
     const colorCodeLength = 6;
